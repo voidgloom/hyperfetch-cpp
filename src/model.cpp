02d8e202022ec652200c080adc0cf8ae54191faf
TotallyRobot <level77zockthd@gmail.com>
2021-11-01 17:39:36 +0100

    model: read product_version too

--- src/model.cpp
+++ src/model.cpp
@@ -7,17 +7,34 @@ void ModelModule::fetch() {
   fseek(f, 0, SEEK_END);
   size_t f_size = ftell(f);
 
-  char *model_contents = new char[f_size];
+  char *product_name = new char[f_size];
 
   rewind(f);
-  fgets(model_contents, f_size, f);
+  fgets(product_name, f_size, f);
 
-  char *ptnewline = strstr(model_contents, "\n");
+  char *ptnewline = strstr(product_name, "\n");
   if (ptnewline != NULL)
     strncpy(ptnewline, "\0", 1);
 
-  prefix = "Device";
-  content = model_contents;
+  fclose(f);
+  f = fopen("/sys/devices/virtual/dmi/id/product_version", "r");
+  fseek(f, 0, SEEK_END);
+  f_size = ftell(f);
 
-  delete[] model_contents;
+  char *product_version = new char[f_size];
+
+  rewind(f);
+  fgets(product_name, f_size, f);
+
+  ptnewline = strstr(product_version, "\n");
+  if (ptnewline != NULL)
+    strncpy(ptnewline, "\0", 1);
+
+
+  prefix = "Device";
+  content = product_name;
+  content += product_version;
+
+  delete[] product_name;
+  delete[] product_version;
 }
