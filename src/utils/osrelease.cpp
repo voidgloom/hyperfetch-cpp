3b71d00bb277d24ae40eb9fea8277128330b5c47
TotallyRobot <level77zockthd@gmail.com>
2021-11-01 17:08:59 +0100

    osrelease: remove quotes and newlines around value

--- src/utils/osrelease.cpp
+++ src/utils/osrelease.cpp
@@ -1,5 +1,6 @@
 #include "osrelease.hpp"
 #include <cstring>
+#include <iostream>
 
 OSReleaseParser::OSReleaseParser() {
     FILE *f = fopen("/etc/os-release", "r");
@@ -9,15 +10,34 @@ OSReleaseParser::OSReleaseParser() {
     char *currentLine = new char[f_size];
     char *token;
 
-
     rewind(f);
     while (fgets(currentLine, f_size, f) != NULL) {
+        // key
         token = strtok(currentLine, "=");
         std::string key = token;
-        token = strtok(currentLine, "=");
-        std::string value = token;
+        // value
+        token = strtok(NULL, "=");
+        // remove quotes and newline from value
+        std::string setToValue = "";
+        int i = 0;
+        int offset = 0;
+        while (token[i] != '\0') {
+            switch (token[i]) {
+                case '\"':
+                    offset++;
+                    break;
+                case '\n':
+                    break;
+                default:
+                    setToValue += token[i];
+            }
+            i++;
+        }
+
+        std::string value = setToValue;
         map.insert(std::pair<std::string, std::string>(key, value));
     }
+    delete[] currentLine;
 }
 
 std::string OSReleaseParser::getValue(std::string key)  {
