12e1b38ca8eebbac2f7c008f132d1dd83dd8c53f
TotallyRobot <level77zockthd@gmail.com>
2021-11-02 18:00:15 +0100

    ascii: add a small logo

--- src/ascii.cpp
+++ src/ascii.cpp
@@ -2,6 +2,10 @@
 #include "utils/osrelease.hpp"
 #include <iostream>
 
+/*
+* ASCII art either official, from neofetch or from pfetch
+*/
+
 void AsciiArt::print() {
     const char *printAscii = std::getenv("HF_ASCII");
     std::string pAscii = "";
@@ -15,37 +19,59 @@ void AsciiArt::print() {
         return;
     }
 
+    const char *smallLogo = std::getenv("HF_SMALL_LOGO");
+        std::string smallLogoStr;
+        if (smallLogo != NULL)
+            smallLogoStr = smallLogo;
+        else
+            smallLogoStr = "";
 
     std::string asciiLogo = "";
     const char *asciiArtEnv = std::getenv("HF_ASCII_LOGO");
     if (asciiArtEnv != NULL) {
         asciiLogo = asciiArtEnv;
+    } else {
+         OSReleaseParser parser;
+        asciiLogo = parser.getValue("ID");
+
     }
 
-    if (asciiLogo.empty()) {
-        OSReleaseParser parser;
-        asciiLogo = parser.getValue("ID");
-    }
+    std::string logo;
 
     if (asciiLogo == "arch") {
-        const char *logo =
-            "               -@\n"
-            "              .##@\n"
-            "             .####@\n"
-            "             @#####@\n"
-            "           . *######@\n"
-            "          .##@o@#####@\n"
-            "         /############@\n"
-            "        /##############@\n"
-            "       @######@**%######@\n"
-            "      @######`     %#####o\n"
-            "     @######@       ######%\n"
-            "   -@#######h       ######@.`\n"
-            "  /#####h**``       `**%@####@\n"
-            " @H@*`                    `*%#@\n"
-            "*`                            `*";
-        width = 34;
-        height = 15;
+        std::string asciiFormat;
+
+        if (smallLogoStr != "1") {
+            logo =
+                "               -@\n"
+                "              .##@\n"
+                "             .####@\n"
+                "             @#####@\n"
+                "           . *######@\n"
+                "          .##@o@#####@\n"
+                "         /############@\n"
+                "        /##############@\n"
+                "       @######@**%######@\n"
+                "      @######`     %#####o\n"
+                "     @######@       ######%\n"
+                "   -@#######h       ######@.`\n"
+                "  /#####h**``       `**%@####@\n"
+                " @H@*`                    `*%#@\n"
+                "*`                            `*";
+            width = 34;
+            height = 15;
+        } else {
+            logo =
+                "       /\\\n"
+				"      /  \\\n"
+				"     /\\   \\\n"
+				"    /      \\\n"
+				"   /   ,,   \\\n"
+				"  /   |  |  -\\\n"
+				" /_-''    ''-_\\";
+            width = 16;
+            height = 7;
+        }
         std::cout << logo << std::endl;
     } else {
         width = 0;
