214886987bd2033baa0b076fa5180ec92b75cfca
TotallyRobot <level77zockthd@gmail.com>
2021-11-02 18:29:14 +0100

    cpu: cache result of cpu model

--- src/cpu.cpp
+++ src/cpu.cpp
@@ -1,8 +1,35 @@
 #include "cpu.hpp"
 #include "utils/cpuid.hpp"
+#include <sstream>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 void CpuModule::fetch() {
     CpuInfo cpu(true);
-    content = cpu.getValue("model name");
+    // check for cached cpu model name
+    std::string cache_path = "/run/user/";
+    std::string userid;
+    std::stringstream stream;
+    stream << geteuid();
+    stream >> userid;
+    cache_path += userid + "/hyperfetch-cpp/cpu";
+    FILE *f;
+    char *cpuModelName = NULL;
+    if ((f = fopen(cache_path.c_str(), "r"))) {
+        cpuModelName = new char[1024];
+        fgets(cpuModelName, 1024, f);
+    }
+    if (cpuModelName != NULL) {
+        content = cpuModelName;
+    } else {
+        content = cpu.getValue("model name");
+        std::string cacheDir = "/run/user/" + userid + "/hyperfetch-cpp";
+        mkdir(cacheDir.c_str(), 0755);
+        FILE *f;
+        if ((f = fopen(cache_path.c_str(), "w"))) {
+            fputs(content.c_str(), f);
+        }
+    }
     prefix = "CPU";
 }
