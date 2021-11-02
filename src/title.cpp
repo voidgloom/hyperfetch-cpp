086901bf6d8a7fe173d84a6cab86b03a94c7778f
TotallyRobot <level77zockthd@gmail.com>
2021-11-02 14:04:27 +0100

    title: add color

--- src/title.cpp
+++ src/title.cpp
@@ -8,8 +8,15 @@ void TitleModule::print() {
 
 void TitleModule::fetch() {
   std::string username = std::getenv("USER");
+  char *titleFormat = std::getenv("HF_TITLE_FORMAT");
+  std::string titleFormatString;
+  if (titleFormat != NULL)
+    titleFormatString = titleFormat;
+  else
+    titleFormatString = "\033[1m\033[38;5;3m";
+
   noSeperator = true;
   utsname unameResult;
   uname(&unameResult);
-  content = username + "@" + unameResult.nodename;
+  content = titleFormatString + username + "\033[0m" + "@" + titleFormatString + unameResult.nodename + "\033[0m";
 }
