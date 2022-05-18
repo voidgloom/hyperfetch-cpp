d4b48fd271018a278f6b1cd5c87d2438e226e15f
TotallyRobot <level77zockthd@gmail.com>
2022-05-18 21:48:25 +0200

    uptime: add macos support

---
+++ src/impl/uptime/uptime_linux.cpp
@@ -0,0 +1,15 @@
+#include <sys/sysinfo.h>
+
+void UptimeModule::fetch() {
+    struct sysinfo info;
+    sysinfo(&info);
+    int uptime_hours = info.uptime / 3600;
+    int uptime_minutes = info.uptime % 3600 / 60;
+
+    if (uptime_hours != 0)
+        content = std::to_string(uptime_hours) + " hours, " + std::to_string(uptime_minutes) + " minutes";
+    else
+        content = std::to_string(uptime_minutes) + " minutes";
+
+    prefix="Uptime";
+}
--- src/uptime.cpp
+++ src/uptime.cpp
@@ -1,16 +1,7 @@
 #include "uptime.hpp"
-#include <sys/sysinfo.h>
 
-void UptimeModule::fetch() {
-    struct sysinfo info;
-    sysinfo(&info);
-    int uptime_hours = info.uptime / 3600;
-    int uptime_minutes = info.uptime % 3600 / 60;
-
-    if (uptime_hours != 0)
-        content = std::to_string(uptime_hours) + " hours, " + std::to_string(uptime_minutes) + " minutes";
-    else
-        content = std::to_string(uptime_minutes) + " minutes";
-
-    prefix="Uptime";
-}
+#ifdef __linux__
+    #include "impl/uptime/uptime_linux.cpp"
+#elifdef __APPLE__
+    #include "impl/uptime/uptime_macos.cpp"
+#endif
