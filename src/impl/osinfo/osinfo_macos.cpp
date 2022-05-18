e13f81a9bf75dd20b116a1f09e005917d0c3063b
TotallyRobot <level77zockthd@gmail.com>
2022-05-18 21:48:06 +0200

    osinfo: add osinfo
    replaces osrelease parser for getting information about the os.
    it still uses osreleaseparser on linux

---
+++ src/impl/osinfo/osinfo_macos.cpp
@@ -0,0 +1,21 @@
+#include <sys/sysctl.h>
+#include <string>
+
+std::string OsInfo::getOsType() {
+    return "macOS";
+}
+
+std::string OsInfo::getOsName() {
+    return "macOS " + getOsVer();
+}
+
+std::string OsInfo::getOsVer() {
+    char *osVerString;
+    size_t len;
+    sysctlbyname("kern.osproductversion", NULL, &len, NULL, 0);
+    osVerString = (char *) malloc(len);
+    sysctlbyname("kern.osproductversion", osVerString, &len, NULL, 0);
+    std::string returnVal = "";
+    returnVal += osVerString;
+    return returnVal;
+}
