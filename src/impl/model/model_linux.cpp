75ac1941c8e0fb3ba6418f8aa69bfdb860db20d7
TotallyRobot <level77zockthd@gmail.com>
2023-01-06 19:14:49 +0100

    add support for getting model information from devicetrees

--- src/impl/model/model_linux.cpp
+++ src/impl/model/model_linux.cpp
@@ -2,7 +2,12 @@
 #include <cstring>
 
 void ModelModule::fetch() {
+  bool uses_devicetree = false;
   FILE *f = fopen("/sys/devices/virtual/dmi/id/product_name", "r");
+  if (f == NULL) {
+    uses_devicetree = true;
+    f = fopen("/sys/firmware/devicetree/base/model", "r");
+  }
   fseek(f, 0, SEEK_END);
   size_t f_size = ftell(f);
 
@@ -22,33 +27,34 @@ void ModelModule::fetch() {
     }
     i++;
   }
-
+  
   fclose(f);
-  f = fopen("/sys/devices/virtual/dmi/id/product_version", "r");
-  fseek(f, 0, SEEK_END);
-  f_size = ftell(f);
+  if (!uses_devicetree) {
+    f = fopen("/sys/devices/virtual/dmi/id/product_version", "r");
+    fseek(f, 0, SEEK_END);
+    f_size = ftell(f);
 
-  char *product_version = new char[f_size];
+    char *product_version = new char[f_size];
 
-  rewind(f);
-  fgets(product_version, f_size, f);
+    rewind(f);
+    fgets(product_version, f_size, f);
 
-  // add a space
-  finalValue += " ";
-  i = 0;
-  while (product_version[i] != '\0') {
-    switch (product_version[i]) {
-      case '\n':
-        break;
-      default:
-        finalValue += product_version[i];
+    // add a space
+    finalValue += " ";
+    i = 0;
+    while (product_version[i] != '\0') {
+      switch (product_version[i]) {
+        case '\n':
+          break;
+        default:
+          finalValue += product_version[i];
+      }
+      i++;
     }
-    i++;
+    delete[] product_version;
   }
-
   prefix = "Device";
   content = finalValue;
 
   delete[] product_name;
-  delete[] product_version;
 }
