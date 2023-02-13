ca7755b6dc50939ab20218b128568d7968d5222e
TotallyRobot <level77zockthd@gmail.com>
2023-02-13 16:59:33 +0100

    GPU_linux: reorder loop and break when first gpu is found

--- src/impl/gpu/gpu_linux.cpp
+++ src/impl/gpu/gpu_linux.cpp
@@ -1,43 +1,42 @@
 
 #include <cstdlib>
 #include <dirent.h>
+#include <iostream>
 #include "cpu.hpp"
 #include "impl/gpu/gpu_pci_list.hpp"
 #include "impl/gpu/gpu_soc_list.hpp"
 #include "cstring"
 #include "utils/wrapper.hpp"
 
-#define PCI_DEV_DIR "/sys/bus/pci/devices"
+#define PCI_DEV_DIR_PATH "/sys/bus/pci/devices"
+#define PCI_DEV_DIR "/sys/bus/pci/devices/"
 
 void GpuModule::fetch() {
     struct dirent *dev;
-    DWrap dir(PCI_DEV_DIR);
+    DWrap dir(PCI_DEV_DIR_PATH);
     while ((dev = readdir(dir)) != NULL) {
         if (dev->d_name[0] == '.')
             continue;
         Wrap<char *> dev_vendor_path(2048);
-        dev_vendor_path[0] = '\0';
         strcat(dev_vendor_path, PCI_DEV_DIR);
-        strcat(dev_vendor_path, "/");
         strcat(dev_vendor_path, dev->d_name);
         strcat(dev_vendor_path, "/vendor");
         FWrap vendor(dev_vendor_path, "r");
         Wrap<char *> dev_vendor(128);
         fgets(dev_vendor, 128, vendor);
+        if (vendor_map.find(dev_vendor.o) == vendor_map.end()) continue;
         Wrap<char *> dev_device_path(2048);
-        dev_device_path[0] = '\0';
         strcat(dev_device_path, PCI_DEV_DIR);
-        strcat(dev_device_path, "/");
         strcat(dev_device_path, dev->d_name);
         strcat(dev_device_path, "/device");
         FWrap device(dev_device_path, "r");
         Wrap<char *> dev_device(128);
         fgets(dev_device, 128, device);
-        if (vendor_map.find(dev_vendor.o) != vendor_map.end()) {
-            std::map<std::string, std::string> map = vendor_map.at(dev_vendor.o);
-            if (map.find(dev_device.o) != map.end()) {
-               content = map[dev_device.o];
-            }
+
+        std::map<std::string, std::string> map = vendor_map.at(dev_vendor.o);
+        if (map.find(dev_device.o) != map.end()) {
+            content = map[dev_device.o];
+            break;
         }
     }
     // on arm socs the gpu might not be connected via pci(e). in that case we check a soc->gpu map.
