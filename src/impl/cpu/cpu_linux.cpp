285a1b0ecf45fc40dfc60d23f20db5e00805ce66
TotallyRobot <level77zockthd@gmail.com>
2023-08-05 23:04:50 +0200

    CPU_linux: fix cache being used wrongly

--- src/impl/cpu/cpu_linux.cpp
+++ src/impl/cpu/cpu_linux.cpp
@@ -4,12 +4,15 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include "impl/cpu/cpu_fallback_model_list.hpp"
+#include "utils/utils.hpp"
 
 void CpuModule::fetch() {
     CpuInfo cpu(true);
+    bool use_cache = false;
     // check for cached cpu model name and read it if it exists
     std::string cache_path = "/run/user/" + std::to_string(getuid()) + "/hyperfetch-cpp/cpu";
     FWrap f(cache_path.c_str(), "r");
+    
     Wrap<char *> cpuModelName(1024);
     if (f.f) {
         struct stat sFile;
@@ -17,10 +20,12 @@ void CpuModule::fetch() {
         if (sFile.st_size != 0) {
             fgets(cpuModelName, 1024, f);
         }
+        use_cache = true;
+
     }
     // set content to cached cpu model name if it exists
     // if it doesn't parse /proc/cpuinfo and write the result into the cache
-    if (cpuModelName != NULL) {
+    if (use_cache) {
         content = cpuModelName.o;
     } else {
         content = cpu.getValue("model name");
